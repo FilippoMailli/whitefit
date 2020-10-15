93fab2c098ea114ab7c8804f9fddcbba3ee01ae0
Filippo Mailli <filippo.mailli@gmail.com>
2020-10-15 16:38:52 +0200

    Prima formattazione navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,66 +22,50 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand torna-home" href="{{ url('/') }}">
-                    LOGO {{ config('app.name', 'WhiteFit') }}
-                </a>
+            <div class="container container-flex" id="container-navbar-principale">
+                <div class="">
+                    <a class="navbar-brand torna-home" href="{{ url('/') }}">
+                        LOGO  || LOGO
+                    </a>
+                </div>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        TEST1
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
                         <li>
-                            {{-- <a href="{{route('/')}}"></a> --}}
-                            HOME</li>
-                        <li>CONTATTI</li>
-                        <li>CHI SIAMO</li>
-                        <!-- Authentication Links -->
-                        {{-- @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                            <a href="{{route('index')}}">HOME</a>
+                        </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                    </ul>
+                </div>
+                <!-- Center Of Navbar -->
+                <!-- Right Side Of Navbar -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest --}}
-
+                        <li>
+                            <a href="{{route('contatti')}}">CONTATTI</a>
+                        </li>
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            CHI SIAMO
+                            </a>
+                            <div class="dropdown-menu p-0" aria-labelledby="dropdownMenu2">
+                                <a class="dropdown-item" id="item-del-menu" href="{{ROUTE('whitefit')}}">WHITEFIT</a>
+                                <a class="dropdown-item" id="item-del-menu" href="{{ROUTE('whiteteam')}}">WHITETEAM</a>
+                            </div>
+                        </div>
                     </ul>
                 </div>
             </div>
         </nav>
-
+    </div>
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
     @yield('script')
 </body>
 </html>
